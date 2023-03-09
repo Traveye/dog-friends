359dca2da7eab4cc82000922b041ecaf9f6ae92d
Big Trees <traveye@protonmail.com>
2023-03-08 19:33:00 -0800

    updating queries.js with queryusers and query user

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,29 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_THOUGHTS = gql`
-  query 
+export const QUERY_USERS = gql`
+  query getUsers {
+    users {
+        _id
+        username
+        password
+        location
+        dogReference{
+            _id
+        }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+    query getUser($userId: ID!) {
+        user(userId: $userId) {
+            _id
+            username
+            password
+            location
+            dogReference{
+                _id
+            }
+        }
+    }
 `;
