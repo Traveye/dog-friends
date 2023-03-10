9e2042bacec26b1b7f333bb96c874484eb3eb05d
Becka McNally <beckamcnally@gmail.com>
2023-03-10 07:25:26 -0800

    adding auth to app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider,
+  createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { setContext } from '@apollo/client/link/context';
 
 import Landing from './pages/Landing';
 import Dashboard from './pages/Dashboard';
 import DogSearch from './components/DogSearch';
 import DogProfile from './components/DogProfile';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
 });
-
-
-
 
 
 function App() {
