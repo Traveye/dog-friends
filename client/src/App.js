ed1e521f2761b8d15f4876731faf7af0c88bee23
Becka McNally <beckamcnally@gmail.com>
2023-03-09 20:59:14 -0800

    adding app.js routes to pages"

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+import Landing from './pages/Landing';
+import Dashboard from './pages/Dashboard';
+import DogSearch from './components/DogSearch';
+import DogProfile from './components/DogProfile';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider>
+      <Router>
+        <Routes>
+            <Route 
+                path="/" 
+                element={<Landing />}
+              />
+            <Route 
+                path="/dashboard" 
+                element={<Dashboard />}
+              />
+            <Route 
+                path="/dogSearch" 
+                element={<DogSearch />}
+              />            
+              <Route 
+              path="/dogProfile" 
+              element={<DogProfile />}
+            />
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
