3eb77521b12f3990f5574721f52659e2c7d8c371
Big Trees <traveye@protonmail.com>
2023-03-11 17:07:02 -0800

    setting map.js to only render the map as a modular component?

--- client/src/components/DogComponents/map.js
+++ client/src/components/DogComponents/map.js
@@ -1,19 +1,9 @@
 // this is the leaflet map component
 // the main responsibility of the "map" component is to render the map and handle interactions with it, such as adding markers or changing the view.
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer,} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-import icon from "leaflet/dist/images/marker-icon.png";
-import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 export default function renderMap() {
-    let DefaultIcon = L.icon({
-        iconUrl: icon,
-        shadowUrl: iconShadow,
-    });
-    
-    L.Marker.prototype.options.icon = DefaultIcon;
-    
     return (
         <MapContainer
         center={[51.505, -0.09]}
@@ -25,11 +15,6 @@ export default function renderMap() {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={[51.505, -0.09]}>
-            <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-        </Marker>
         </MapContainer>
     );
 };
