84531cdc9f7a983e7b4d2eb14b527e9a2e8f2b85
Big Trees <traveye@protonmail.com>
2023-03-11 16:25:14 -0800

    adding jest test fo user, adding intial comments for dog search

--- client/src/pages/DogSearch.js
+++ client/src/pages/DogSearch.js
@@ -13,51 +13,30 @@ import { useLazyQuery } from "@apollo/react-hooks";
 
 import { QUERY_DOGS } from "../utils/queries";
 
+
+// this page needs to render a map with leaflet all the dogs in the database will be pins on the map -- need to get location information from the database and render it on the map
+// should call car component and render it on the map
+// should have a search bar that allows you to search by location and render the dogs in that location on the map
+// should have a filter that allows you to search by breed and render the dogs of that breed on the map
+// should have a filter that allows you to search by play style and render the dogs of that play style on the map
+// should have a filter that allows you to search by age and render the dogs of that age on the map
+// should have a filter that allows you to search by size and render the dogs of that size on the map
+
 const DogSearch = () => {
-  const [searchedDogs, setSearchedDogs] = useState([]);
-  const [searchLocation, setSearchLocation] = useState("");
+  //this is to keep track of the search input
+  const [search, setSearch] = useState("");
+  //this is to keep track of the search results
+  const [searchResults, setSearchResults] = useState([]);
+  
+  //this is to return the dogs from the database
   const [getDogs, { loading, data }] = useLazyQuery(QUERY_DOGS);
 
-  const handleChange = (event) => {
-    setSearchLocation(event.target.value);
-  };
+  //this is to handle the search input
+  const handleInputChange = (event) => {};
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    getDogs({ variables: { location: searchLocation } });
-  };
+  //this is to handle the search submit
+  const handleFormSubmit = (event) => {};
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  const dogs = data?.dogs || [];
-
-  setSearchedDogs(dogs);
-
-  return (
-    <div>
-      <h2>Search for a dog</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Search for a dog by location"
-          value={searchLocation}
-          onChange={handleChange}
-        />
-        <button type="submit">Search</button>
-      </form>
-      <div>
-        {searchedDogs.map((dog) => (
-          <div key={dog._id}>
-            <h3>{dog.name}</h3>
-            <p>{dog.breed}</p>
-            <p>{dog.location}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  //this is to render the search results to the leaflet map
+  const renderSearchResults = () => {};
 };
-
-export default DogSearch;
