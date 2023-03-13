a78a4c8339ac6b09eff372a0051f11d70b42aa84
Big Trees <traveye@protonmail.com>
2023-03-13 02:29:11 -0700

    adding pre hook to dog model for geocoding/ commenting out until run seeds is funcitonal again

--- server/models/Dog.js
+++ server/models/Dog.js
@@ -1,4 +1,7 @@
 const { Schema, model } = require('mongoose');
+const fetch = require('node-fetch');
+const MAPBOX_TOKEN = "pk.eyJ1IjoidHJhdmV5ZSIsImEiOiJjbGY2aXRhdmgxbWYwM3FycW53eHVnOW1lIn0.VvfYmU6HQEsz17zN4ly0EA"
+//process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 const endorsementsSchema = new Schema({
    playStyle:{
@@ -37,6 +40,27 @@ const dogSchema = new Schema({
       endorsementsSchema
    }]
 });
+
+// dogSchema.pre('save', async function (next) {
+//    const dog = this;
+//    if (!dog.isModified('location')) {
+//      return next();
+//    }
+ 
+//    try {
+//      const response = await fetch(
+//        `https://api.mapbox.com/geocoding/v5/mapbox.places/${dog.location}.json?access_token=${MAPBOX_TOKEN}`
+//      );
+//      const data = await response.json();
+//      const [longitude, latitude] = data.features[0].center;
+ 
+//      dog.longitude = longitude;
+//      dog.latitude = latitude;
+//      next();
+//    } catch (error) {
+//      next(error);
+//    }
+//  });
 
 const Dog = model('Dog', dogSchema);
 
